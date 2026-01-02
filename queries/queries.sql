d2462bedb11c38aade7cfba5d678b15f0b197ad8
gregoridelrio <gregoridelrio@gmail.com>
2026-01-02 20:00:00 +0100

    add 9

--- queries/queries.sql
+++ queries/queries.sql
@@ -63,7 +63,15 @@ ON g.id = a.id_grado
 WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
 
 -- 9. Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019. (nombre, apellido1, apellido2)
-
+SELECT DISTINCT p.nombre, p.apellido1, p.apellido2
+FROM persona p
+JOIN alumno_se_matricula_asignatura al
+ON p.id = al.id_alumno
+JOIN asignatura a
+ON al.id_asignatura = a.id
+JOIN curso_escolar c
+ON c.id = al.id_curso_escolar
+WHERE c.anyo_inicio = 2018;
 
 -- Resol les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN.
 -- 10. Retorna un llistat amb els noms de tots els professors/es i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors/es que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor/a. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom. (departamento, apellido1, apellido2, nombre)
