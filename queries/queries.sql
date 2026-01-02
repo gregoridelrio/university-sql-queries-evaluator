f71e94eb7a5038603c016f03463c9766dec61855
gregoridelrio <gregoridelrio@gmail.com>
2026-01-02 15:24:25 +0100

    add 4, 5

--- queries/queries.sql
+++ queries/queries.sql
@@ -17,10 +17,18 @@ WHERE tipo = 'alumno'
 AND YEAR(fecha_nacimiento) = 1999;
 
 -- 4. Retorna el llistat de professors/es que no han donat d'alta el seu número de telèfon en la base de dades i a més el seu NIF acaba en K. (nombre, apellido1, apellido2, nif)
-
+SELECT nombre, apellido1, apellido2, nif
+FROM persona
+WHERE tipo = 'profesor'
+AND telefono IS NULL
+AND NIF LIKE '%K';
 
 -- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7. (id, nombre, cuatrimestre, curso, id_grado)
-
+SELECT id, nombre, cuatrimestre, curso, id_grado
+FROM asignatura
+WHERE cuatrimestre = 1
+AND curso = 3
+AND id_grado = 7;
 
 -- 6. Retorna un llistat dels professors/es juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom. (apellido1, apellido2, nombre, departamento)
 
