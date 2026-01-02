9e5fa8a14dcefc5585ea3aff4c61745eac163e34
gregoridelrio <gregoridelrio@gmail.com>
2026-01-02 13:18:22 +0100

    add 1

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,5 +1,8 @@
 -- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
-
+SELECT apellido1, apellido2, nombre
+FROM persona
+WHERE tipo = 'alumno'
+ORDER BY apellido1 ASC, apellido2 ASC, nombre ASC;
 
 -- 2. Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades. (nombre, apellido1, apellido2)
 
