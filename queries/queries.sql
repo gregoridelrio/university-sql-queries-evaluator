4872bf189199ba2bdadce47b55cb2491550058e5
gregoridelrio <gregoridelrio@gmail.com>
2026-01-05 11:46:21 +0100

    add 22

--- queries/queries.sql
+++ queries/queries.sql
@@ -166,7 +166,11 @@ GROUP BY g.nombre
 HAVING total > 40;
 
 -- 22. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus. (grau, tipus, total_creditos)
-
+SELECT g.nombre AS grau, a.tipo AS tipus, SUM(creditos) AS total_creditos
+FROM grado g
+JOIN asignatura a
+ON a.id_grado = g.id
+GROUP BY g.nombre, a.tipo;
 
 -- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats. (anyo_inicio, total)
 
